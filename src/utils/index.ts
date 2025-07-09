5e7026943656f305061b45d4ea33b34cb8ae2d57
cq <932798303@qq.com>
2025-07-09 16:54:56 +0800

    add Routes

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,21 @@
 const a = 1
+
+function FN<T>(params: T): T {
+  return params
+}
+
+type User = {
+  name: 'cq'
+  age: 18
+}
+
+type UserKey = keyof User
+
+const name1: UserKey = 'name'
+
+const user = {
+  a: 1,
+  b: 2
+}
+
+type AAA = typeof user
