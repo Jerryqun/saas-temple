8b60e6b06bfa6dcfa9cbd9fd9a6cfbd95320666e
cq <932798303@qq.com>
2025-07-10 20:02:46 +0800

    storage

---
+++ src/utils/storage.ts
@@ -0,0 +1,23 @@
+export default {
+  set(key: string, value: unknown) {
+    localStorage.setItem(key, JSON.stringify(value))
+  },
+  get(key: string) {
+    const value = localStorage.getItem(key)
+    if (!value) {
+      return ''
+    }
+    try {
+      return JSON.parse(value)
+    } catch (err) {
+      console.log('err: ', err)
+      return value
+    }
+  },
+  remove(key: string) {
+    return localStorage.removeItem(key)
+  },
+  clear() {
+    return localStorage.clear()
+  }
+}
