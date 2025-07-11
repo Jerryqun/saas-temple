e8aa58f74f0b138e310dcbab814d65b2b7a73128
cq <932798303@qq.com>
2025-07-11 11:09:02 +0800

    时间格式化

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,10 @@
 import { useEffect } from 'react'
 import request from '@/utils/request'
+import { formatDate } from '@/utils'
 
 export default () => {
   console.log('import.meta.env', import.meta.env)
+  console.log('time---', formatDate('2024-7-1321 11:4:19'))
   useEffect(() => {
     request.get('/login', { name: 'cq' })
   }, [])
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 /**
- * 格式化金额
+ *
  * @param num
  * @returns
  */
@@ -18,4 +18,58 @@ export const formatNum = (num?: number | string) => {
   const a = num.toString()
   if (a.indexOf('.') > -1) return a.replace(/(\d)(?=(\d{3})+\.)/g, '$1,')
   return a.replace(/(\d)(?=(\d{3})+$)/g, '$1,')
+}
+
+/**
+ * 格式化时间
+ * @param date
+ * @param rule
+ * @returns
+ */
+
+export const toLocalDate = (date?: Date, rule?: string) => {
+  let cur = new Date()
+  if (date) cur = date
+  if (rule === 'yyyy-MM-dd') {
+    return cur?.toLocaleDateString().replaceAll('/', '-')
+  }
+  if (rule === 'HH:mm:ss') {
+    return cur.toLocaleTimeString().replaceAll('/', '-')
+  }
+  return cur.toLocaleString().replaceAll('/', '-')
+}
+
+/**
+ * 时间格式化
+ * @param date
+ * @param rule
+ * @returns
+ */
+export const formatDate = (date?: Date | string, rule?: string) => {
+  let cur = new Date()
+  if (date instanceof Date) {
+    cur = date
+  } else if (date) {
+    cur = new Date(date)
+    if (cur.toString() === 'Invalid Date') {
+      return '请输入正确的时间格式'
+    }
+  }
+
+  let fmt = rule || 'yyyy-MM-dd HH:mm:ss'
+  const O: {
+    [key: string]: number
+  } = {
+    yyyy: cur.getFullYear(),
+    'M+': cur.getMonth() + 1,
+    'd+': cur.getDate(),
+    'H+': cur.getHours(),
+    'm+': cur.getMinutes(),
+    's+': cur.getSeconds()
+  }
+  for (const k in O) {
+    const val = O[k].toString()
+    fmt = fmt.replace(new RegExp(`(${k})`), val)
+  }
+  return fmt
 }
