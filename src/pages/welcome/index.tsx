d76ad4dd872764d286fd88dd3f4d65830ff34863
cq <932798303@qq.com>
2025-07-15 10:35:37 +0800

    bg

--- src/pages/welcome/index.tsx
+++ src/pages/welcome/index.tsx
@@ -1,7 +1,9 @@
+import { Button } from 'antd'
 import styles from './index.module.css'
 export default function Login() {
   return (
     <div className={styles.welcome}>
+      <Button type='link'>Link Button</Button>
       <div className={styles.content}>
         <div className={styles.subTitle}>欢迎体验</div>
         <div className={styles.title}>React18通用后台管理系统</div>
