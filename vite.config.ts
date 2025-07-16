3efa03aed8dbf4b787803a10dfe18af30e18c981
cq <932798303@qq.com>
2025-07-16 16:09:35 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,22 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-	root: "./",
-	// base: "api", // 默认
-	// publicDir: "./public", // // 默认
-	plugins: [react()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
-	// server: {
-	// 	host: "localhost",
-	// 	port: 9999,
-	// 	proxy: {},
-	// },
-});
+  root: './',
+  // base: "api", // 默认
+  // publicDir: "./public", // // 默认
+  plugins: [react()],
+  base: '/saas-temple/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+  // server: {
+  // 	host: "localhost",
+  // 	port: 9999,
+  // 	proxy: {},
+  // },
+})
