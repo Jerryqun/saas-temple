e2136bdd13cf0e3c8d9bed9e910416b5c23065fc
cq <932798303@qq.com>
2025-07-16 16:49:42 +0800

    fix

--- src/store/index.ts
+++ src/store/index.ts
@@ -30,8 +30,7 @@ export const useStore = create<useStoreType>(set => ({
     roleList: '',
     createId: 0,
     deptName: '财务部',
-    userImg:
-      'https://wisdomhammer.oss-cn-hangzhou.aliyuncs.com/pmsaas/1025/1/20230721/02be82d4e18bc3e7e155e1f744cf4586.jpeg?Expires=3267854213&OSSAccessKeyId=LTAI5t5fwTBbu4WT1PJCaeCS&Signature=MM2nS5lj6%2FEr7X%2FKkbeEP15bKsc%3D'
+    userImg: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg'
   },
   collapsed: false,
   isDark: storage.get('isDark') || false,
